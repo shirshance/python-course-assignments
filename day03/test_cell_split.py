cd564ed939fe0b6329df7ed9d886b727de872726
shirshance <61277252+shirshance@users.noreply.github.com>
2026-04-30 17:55:46 +0300

    Update test_cell_split.py

--- day03/test_cell_split.py
+++ day03/test_cell_split.py
@@ -1,16 +1,16 @@
-from cell_split_lib import calculate_split
+from cell_split_lib import split_cells
 
 def test_split_1_to_10():
-    cells, medium = calculate_split(10, 10)
+    cells, medium = split_cells(10, 10)
     assert cells == 1
     assert medium == 9
 
 def test_split_1_to_5():
-    cells, medium = calculate_split(10, 5)
+    cells, medium = split_cells(10, 5)
     assert cells == 2
     assert medium == 8
 
 def test_split_1_to_3():
-    cells, medium = calculate_split(12, 3)
+    cells, medium = split_cells(12, 3)
     assert cells == 4
     assert medium == 8
